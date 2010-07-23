611235d6a392b2e0d1fec08315b459cf14966a2f
Oaz <github@azeau.com>
2010-07-23 22:23:04 +0200

    refactored regex spike

--- Specs/Moq/RegexSpike.cs
+++ Specs/Moq/RegexSpike.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using NUnit.Framework;
 
@@ -10,65 +11,97 @@ namespace Specs.Oaz.SpecFlowHelpers.Moq
 		[Test]
 		public void FunctionCallStyle ()
 		{	
-			var m0 = Regex.Match( "aa(bb,cc,dd,,ee)", "(.*)\\(" );
-			Assert.That( m0.Groups[1].Value, Is.EqualTo("aa") );
-			
-			var m1 = Regex.Matches( "aa(bb,cc,dd,,ee)", "[,\\(]([^,\\)]*)" );
-			Assert.That( m1.Count, Is.EqualTo(5) );
-			Assert.That( m1[0].Groups[1].Value, Is.EqualTo("bb") );
-			Assert.That( m1[1].Groups[1].Value, Is.EqualTo("cc") );
-			Assert.That( m1[2].Groups[1].Value, Is.EqualTo("dd") );
-			Assert.That( m1[3].Groups[1].Value, Is.EqualTo("") );
-			Assert.That( m1[4].Groups[1].Value, Is.EqualTo("ee") );
+			Check
+			.Match("aa(bb,cc,dd,,ee)")
+			.Command("(.*)\\(", "aa")
+			.Parameters("[,\\(]([^,\\)]*)", 0, "bb", "cc", "dd", "", "ee");
 		}
 		
 		[Test]
 		public void FunctionCallStyleWithResult()
 		{	
-			var m0 = Regex.Match( "aa(bb,cc,dd,,ee)=zz", "(.*)\\(" );
-			Assert.That( m0.Groups[1].Value, Is.EqualTo("aa") );
-			
-			var m1 = Regex.Matches( "aa(bb,cc,dd,,ee)=zz", "[,\\(]([^,\\)]*)" );
-			Assert.That( m1.Count, Is.EqualTo(5) );
-			Assert.That( m1[0].Groups[1].Value, Is.EqualTo("bb") );
-			Assert.That( m1[1].Groups[1].Value, Is.EqualTo("cc") );
-			Assert.That( m1[2].Groups[1].Value, Is.EqualTo("dd") );
-			Assert.That( m1[3].Groups[1].Value, Is.EqualTo("") );
-			Assert.That( m1[4].Groups[1].Value, Is.EqualTo("ee") );
-			
-			var m2 = Regex.Match( "aa(bb,cc,dd,,ee)=zz", "=(.*)" );
-			Assert.That( m2.Groups[1].Value, Is.EqualTo("zz") );;
+			Check
+			.Match("aa(bb,cc,dd,,ee)=zz")
+			.Command("(.*)\\(", "aa")
+			.Parameters("[,\\(]([^,\\)]*)", 0, "bb", "cc", "dd", "", "ee")
+			.Result("=(.*)", "zz");
 		}
 		
 		[Test]
 		public void SpacesStyle ()
 		{	
-			var m0 = Regex.Match( "aa bb cc dd ee", "^(.*?) " );
-			Assert.That( m0.Groups[1].Value, Is.EqualTo("aa") );
-			
-			var m1 = Regex.Matches( "aa bb cc dd ee", " ([^ ]+)" );
-			Assert.That( m1.Count, Is.EqualTo(4) );
-			Assert.That( m1[0].Groups[1].Value, Is.EqualTo("bb") );
-			Assert.That( m1[1].Groups[1].Value, Is.EqualTo("cc") );
-			Assert.That( m1[2].Groups[1].Value, Is.EqualTo("dd") );
-			Assert.That( m1[3].Groups[1].Value, Is.EqualTo("ee") );
+			Check
+			.Match("aa bb cc dd ee")
+			.Command("^(.*?) ", "aa")
+			.Parameters(" ([^ ]+)", 0, "bb", "cc", "dd", "ee");
 		}
 		
 		[Test]
 		public void SpacesStyleWithResult ()
-		{	
-			var m0 = Regex.Match( "aa bb cc dd ee => zz", "^(.*?) " );
-			Assert.That( m0.Groups[1].Value, Is.EqualTo("aa") );
+		{
+			Check
+			.Match("aa bb cc dd ee => zz")
+			.Command("^(.*?) ", "aa")
+			.Parameters(" ([^ ]+)", 2, "bb", "cc", "dd", "ee")
+			.Result("=> *(.*)", "zz");
+		}
+		
+		[Test]
+		public void SentenceStyle ()
+		{
+			Check
+			.Match("aa aa \"b b\" 3 \"d d\" 4")
+			.Command("^(.*?) [\"0-9]", "aa aa")
+			.Parameters(" (\"(.*?)\"|[0-9]+)", 0, "b b", "3", "d d", "4");
 			
-			var m1 = Regex.Matches( "aa bb cc dd ee => zz", " ([^ ]+)" );
-			Assert.That( m1.Count, Is.EqualTo(6) );
-			Assert.That( m1[0].Groups[1].Value, Is.EqualTo("bb") );
-			Assert.That( m1[1].Groups[1].Value, Is.EqualTo("cc") );
-			Assert.That( m1[2].Groups[1].Value, Is.EqualTo("dd") );
-			Assert.That( m1[3].Groups[1].Value, Is.EqualTo("ee") );
-			
-			var m2 = Regex.Match( "aa bb cc dd ee => zz", "=> *(.*)" );
-			Assert.That( m2.Groups[1].Value, Is.EqualTo("zz") );;
+			Check
+			.Match("aa aa 3 \"b b\" 4 \"d d\"")
+			.Command("^(.*?) [\"0-9]", "aa aa")
+			.Parameters(" (\"(.*?)\"|[0-9]+)", 0, "3", "b b", "4", "d d");
+		}
+	}
+	
+	static class Check
+	{
+		public static Checker Match(string text)
+		{
+			return new Checker() { Text=text };
+		}
+	}
+	
+	class Checker
+	{
+		public string Text  {get;set;}
+
+		public Checker Command(string regex, string val)
+		{
+			return RegexMatch( regex, val, "Wrong Command" );
+		}
+		
+		public Checker Parameters(string regex, int delta, params string[] values)
+		{
+			var matches = Regex.Matches( Text, regex );
+			Assert.That( matches.Count, Is.EqualTo(values.Length+delta) );
+			for(int i=0; i<values.Length; i++)
+				Assert.That( MatchValue(matches[i]), Is.EqualTo(values[i]), string.Format("Wrong Parameter {0}",i) );
+			return this;
+		}
+		
+		public Checker Result(string regex, string val)
+		{
+			return RegexMatch( regex, val, "Wrong Result" );
+		}
+
+		private Checker RegexMatch(string regex, string val, string message)
+		{
+			var match = Regex.Match( Text, regex );
+			Assert.That( MatchValue(match), Is.EqualTo(val), message );
+			return this;
+		}
+		
+		private static string MatchValue(Match m)
+		{
+			return m.Groups.Cast<Group>().Last(g=>g.Success).Value;
 		}
 	}
 }
