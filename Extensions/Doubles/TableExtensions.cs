507cb42d2dc5e8e6277adc28a519938741a88c82
Oaz <github@azeau.com>
2010-07-25 21:33:17 +0200

    minor cleaning on verifications

--- Extensions/Doubles/TableExtensions.cs
+++ Extensions/Doubles/TableExtensions.cs
@@ -9,11 +9,6 @@ namespace Oaz.SpecFlowHelpers.Doubles
 	{
 		public static IEnumerable<Command<T>> AsCommands<T>(this Table table, CommandLanguage language) where T:class
 		{
-			return AsCommandsImpl<T>(table,language).ToArray().AsEnumerable();
-		}
-		
-		private static IEnumerable<Command<T>> AsCommandsImpl<T>(Table table, CommandLanguage language) where T:class
-		{
 			foreach(var row in table.Rows)
 				yield return language.Command<T>(row[0]);
 		}
